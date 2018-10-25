5218718eaed6c580f4eaaf0e8c601c30c596bca5
Steve Farrell <sparticlesteve@gmail.com>
2018-10-25 15:13:48 -0700

    adding dropout knob to resnet

--- models/resnet.py
+++ models/resnet.py
@@ -9,21 +9,21 @@ https://github.com/keras-team/keras-contrib/blob/master/keras_contrib/applicatio
 import keras
 from keras_contrib.applications import resnet
 
-def build_resnet18_cifar(input_shape=(32, 32, 3), n_classes=10):
+def build_resnet18_cifar(input_shape=(32, 32, 3), n_classes=10, dropout=None):
     """Build the resnet18 model with appropriate settings for CIFAR10"""
 
     # These are the recommended settings for CIFAR10 from
     # keras_contrib/applications/resnet.py
-    model = resnet.ResNet(input_shape=input_shape,
-                          classes=n_classes,
-                          block='basic',
-                          repetitions=[2, 2, 2, 2],
-                          include_top=True,
-                          initial_strides=(1, 1),
-                          initial_kernel_size=(3, 3),
-                          initial_pooling=None,
-                          top='classification')
-    return model
+    return resnet.ResNet(input_shape=input_shape,
+                         classes=n_classes,
+                         block='basic',
+                         repetitions=[2, 2, 2, 2],
+                         include_top=True,
+                         dropout=dropout,
+                         initial_strides=(1, 1),
+                         initial_kernel_size=(3, 3),
+                         initial_pooling=None,
+                         top='classification')
 
 def _test():
     model = build_resnet18_cifar()
