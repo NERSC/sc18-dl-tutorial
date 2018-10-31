4a07d2870c8c2fa23da7ce1cd3ed4ac3c8933a2f
Steve Farrell <sparticlesteve@gmail.com>
2018-10-30 22:07:17 -0700

    improved batchScript

--- scripts/batchScript.sh
+++ scripts/batchScript.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+#SBATCH -J cifar10
 #SBATCH -C haswell
 #SBATCH -N 1
 #SBATCH -q debug
 #SBATCH -t 30
-#SBATCH -o logs/cifar10-%j.out
+#SBATCH -o logs/%x-%j.out
 
 config=configs/cifar10_cnn.yaml
 if [ $# -gt 0 ]; then config=$1; fi
 
 . scripts/setup.sh
-srun -l python train.py $config --distributed
+srun -u -l python train.py $config --distributed
