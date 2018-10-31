7c7f2fba4bd03aeaa30ca3e5fbe13e022d1c76d7
Steve Farrell <sparticlesteve@gmail.com>
2018-10-31 11:45:09 -0700

    made thread-setting more configurable
    Updated the configure_session function with more arguments.
    The function now also sets the KMP_BLOCKTIME and KMP_AFFINITY.

--- utils/device.py
+++ utils/device.py
@@ -5,13 +5,19 @@ Hardware/device configuration
 # System
 import os
 
-# Haswell settings for now
-def configure_session():
-    os.environ['OMP_NUM_THREADS'] = '32'
-    import keras
-    import tensorflow as tf
+# Externals
+import keras
+import tensorflow as tf
+
+# Default settings are for Haswell
+def configure_session(intra_threads=32, inter_threads=2,
+                      blocktime=1, affinity='granularity=fine,compact,1,0'):
+    """Sets the thread knobs in the TF backend"""
+    os.environ['KMP_BLOCKTIME'] = str(blocktime)
+    os.environ['KMP_AFFINITY'] = affinity
+    os.environ['OMP_NUM_THREADS'] = str(intra_threads)
     config = tf.ConfigProto(
-        inter_op_parallelism_threads=2,
-        intra_op_parallelism_threads=32
+        inter_op_parallelism_threads=inter_threads,
+        intra_op_parallelism_threads=intra_threads
     )
     keras.backend.set_session(tf.Session(config=config))
