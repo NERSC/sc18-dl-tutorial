d53ca2a9ce68390cf8f56498abf7284f3ac750f9
Steve Farrell <sparticlesteve@gmail.com>
2018-10-30 22:06:37 -0700

    added imagenet specification

--- data/__init__.py
+++ data/__init__.py
@@ -10,5 +10,8 @@ def get_datasets(name, **data_args):
     elif name == 'cifar10':
         from .cifar10 import get_datasets
         return get_datasets(**data_args)
+    elif name == 'imagenet':
+        from .imagenet import get_datasets
+        return get_datasets(**data_args)
     else:
         raise ValueError('Dataset %s unknown' % name)
