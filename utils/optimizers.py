edd93e4fe29a6f12230ae1f0422c5d1dde245998
Steve Farrell <sparticlesteve@gmail.com>
2018-11-03 21:51:59 -0700

    added support for sqrt(N) scaling of LR

--- utils/optimizers.py
+++ utils/optimizers.py
@@ -9,16 +9,17 @@ import math
 import keras
 import horovod.keras as hvd
 
-def get_optimizer(name, lr, n_ranks=1, scale_lr=True,
+def get_optimizer(name, lr, lr_scaling='linear', n_ranks=1,
                   distributed=False, **opt_args):
     """
     Configure the optimizer and scale the learning rate by n_ranks.
-    TODO: add support for sqrt scaling of learning rate.
     TODO: add support for wrapping TF optimizers like LARS.
     """
     # Scale the learning rate
-    if scale_lr:
+    if lr_scaling == 'linear':
         lr = lr * n_ranks
+    elif lr_scaling == 'sqrt':
+        lr = lr * math.sqrt(n_ranks)
 
     # Construct the optimizer
     OptType = getattr(keras.optimizers, name)
