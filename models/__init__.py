d48ea34057d9ffac92573ea16d7215f5c7c38ba3
Steve Farrell <sparticlesteve@gmail.com>
2018-11-05 23:54:30 -0800

    added official keras resnet50 with some modifications

--- models/__init__.py
+++ models/__init__.py
@@ -6,11 +6,14 @@ def get_model(name, **model_args):
     if name == 'cnn':
         from .cnn import build_model
         return build_model(**model_args)
-    if name == 'resnet18_cifar':
+    elif name == 'resnet18_cifar':
         from .resnet import build_resnet18_cifar
         return build_resnet18_cifar(**model_args)
-    if name == 'resnet50':
+    elif name == 'resnet50':
         from .resnet import build_resnet50
+        return build_resnet50(**model_args)
+    elif name == 'resnet50_official':
+        from .resnet_official import build_resnet50
         return build_resnet50(**model_args)
     else:
         raise ValueError('Model %s unknown' % name)
