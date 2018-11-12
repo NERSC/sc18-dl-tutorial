934d4983d35225296e37564299afa7c0a0ce9d71
Steve Farrell <sparticlesteve@gmail.com>
2018-11-11 20:10:44 -0800

    moved from constructor form to model.add for simple cnn

--- models/cnn.py
+++ models/cnn.py
@@ -10,16 +10,15 @@ from keras.layers import Conv2D, MaxPooling2D
 def build_model(input_shape=(32, 32, 3), n_classes=10, dropout=0):
     """Construct the simple CNN model"""
     conv_args = dict(kernel_size=3, padding='same', activation='relu')
-    model = Sequential([
-        Conv2D(16, input_shape=input_shape, **conv_args),
-        MaxPooling2D(pool_size=2),
-        Conv2D(32, **conv_args),
-        MaxPooling2D(pool_size=2),
-        Conv2D(64, **conv_args),
-        MaxPooling2D(pool_size=2),
-        Flatten(),
-        Dense(128, activation='relu'),
-        Dropout(dropout),
-        Dense(n_classes, activation='softmax')
-    ])
+    model = Sequential()
+    model.add(Conv2D(16, input_shape=input_shape, **conv_args))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Conv2D(32, **conv_args))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Conv2D(64, **conv_args))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(dropout))
+    model.add(Dense(n_classes, activation='softmax'))
     return model
